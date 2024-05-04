c5135b122181d716e9e12d1101a0435a9089c46d
Arbaj Ansari <83424578+ArbajAr7@users.noreply.github.com>
2024-05-04 22:09:27 +0545

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,39 @@
 //complete this code
-class Animal {}
+class Animal {
+	constructor(species) {
+		this.species =species;
+	}
+	get getSpecies() {
+		return this.species;
+	}
+	makeSound() {
+		console.log("makes a sound");
+	}
+}
 
-class Dog extends Animal {}
+class Dog extends Animal {
+	constructor(name){
+		super(name);
+	}
+	makeSound() {
+		console.log(this.name+" makes a sound");
+	}
+	bark() {
+		console.log("woof");
+	}
+}
 
-class Cat extends Animal {}
+class Cat extends Animal {
+	constructor(name){
+		super(name);
+	}
+	makeSound() {
+		console.log(this.name+" makes a sound");
+	}
+	purr() {
+		console.log("woof");
+	}
+}
 
 // Do not change the code below this line
 window.Animal = Animal;
